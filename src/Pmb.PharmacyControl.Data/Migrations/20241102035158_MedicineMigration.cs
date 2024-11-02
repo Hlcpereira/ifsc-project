ddafbbdb2233263d3ff24649ca65e093608c10f1
Henrique Pereira <hlcpereira@outlook.com.br>
2024-11-02 01:16:46 -0300

    Pmb.Pharmacy.Data: Migrations, migrations, migrations...
    Signed-off-by: Henrique Pereira <hlcpereira@outlook.com.br>

---
+++ src/Pmb.PharmacyControl.Data/Migrations/20241102035158_MedicineMigration.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Pmb.PharmacyControl.Data.Migrations
+{
+    public partial class MedicineMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "public");
+
+            migrationBuilder.CreateTable(
+                name: "medicine",
+                schema: "public",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    control_level = table.Column<short>(type: "smallint", nullable: false),
+                    integration_url = table.Column<string>(type: "varchar", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_medicine", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "medicine",
+                schema: "public");
+        }
+    }
+}
