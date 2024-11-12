f0e599dd540fd2b0ad7df1a1fd3bc583b0531bb4
Henrique Pereira <hlcpereira@outlook.com.br>
2024-11-12 17:03:49 -0300

    Pmb.PharmacyControl.Data: Migrations... Migrations never change
    Signed-off-by: Henrique Pereira <hlcpereira@outlook.com.br>

---
+++ src/Pmb.PharmacyControl.Data/Migrations/20241112190305_PharmaceuticalMigration.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Pmb.PharmacyControl.Data.Migrations
+{
+    public partial class PharmaceuticalMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "integration_url",
+                schema: "public",
+                table: "medicine",
+                newName: "name");
+
+            migrationBuilder.CreateTable(
+                name: "pharmaceutical",
+                schema: "public",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    name = table.Column<string>(type: "varchar", nullable: true),
+                    register_number = table.Column<string>(type: "varchar", nullable: true),
+                    health_unit_id = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_pharmaceutical", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "pharmaceutical",
+                schema: "public");
+
+            migrationBuilder.RenameColumn(
+                name: "name",
+                schema: "public",
+                table: "medicine",
+                newName: "integration_url");
+        }
+    }
+}
