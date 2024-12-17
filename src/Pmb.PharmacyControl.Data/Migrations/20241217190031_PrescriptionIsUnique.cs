d487c573d02f77bd432b71e242122cbf0d4f78e9
Henrique Pereira <hlcpereira@outlook.com.br>
2024-12-17 16:01:18 -0300

    Pmb.PharmacyControl: Data: Migrations (17/Dec/2024)
    Signed-off-by: Henrique Pereira <hlcpereira@outlook.com.br>

---
+++ src/Pmb.PharmacyControl.Data/Migrations/20241217190031_PrescriptionIsUnique.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Pmb.PharmacyControl.Data.Migrations
+{
+    public partial class PrescriptionIsUnique : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_medicine_control_prescription_url",
+                schema: "public",
+                table: "medicine_control",
+                column: "prescription_url",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_medicine_control_prescription_url",
+                schema: "public",
+                table: "medicine_control");
+        }
+    }
+}
