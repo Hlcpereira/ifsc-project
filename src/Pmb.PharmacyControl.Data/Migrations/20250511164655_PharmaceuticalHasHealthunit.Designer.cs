138ac1f921c195a6c86ae36e8d6860d4afe2988c
Henrique Pereira <hlcpereira@outlook.com.br>
2025-05-11 14:08:08 -0300

    Pmb.PharmacyControl.Data: Migration time~
    Signed-off-by: Henrique Pereira <hlcpereira@outlook.com.br>

--- src/Pmb.PharmacyControl.Data/Migrations/20250511164655_PharmaceuticalHasHealthunit.Designer.cs
+++ src/Pmb.PharmacyControl.Data/Migrations/20250511164655_PharmaceuticalHasHealthunit.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Pmb.PharmacyControl.Data;
@@ -11,9 +12,10 @@ using Pmb.PharmacyControl.Data;
 namespace Pmb.PharmacyControl.Data.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20250511164655_PharmaceuticalHasHealthunit")]
+    partial class PharmaceuticalHasHealthunit
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/Pmb.PharmacyControl.Data/Migrations/20250511164655_PharmaceuticalHasHealthunit.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Pmb.PharmacyControl.Data.Migrations
+{
+    public partial class PharmaceuticalHasHealthunit : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_pharmaceutical_health_unit_id",
+                schema: "public",
+                table: "pharmaceutical",
+                column: "health_unit_id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_pharmaceutical_health_nit_health_unit_id",
+                schema: "public",
+                table: "pharmaceutical",
+                column: "health_unit_id",
+                principalSchema: "public",
+                principalTable: "health_nit",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_pharmaceutical_health_nit_health_unit_id",
+                schema: "public",
+                table: "pharmaceutical");
+
+            migrationBuilder.DropIndex(
+                name: "IX_pharmaceutical_health_unit_id",
+                schema: "public",
+                table: "pharmaceutical");
+        }
+    }
+}
